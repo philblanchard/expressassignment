4bf354043066d7d5bc160c686b3202ae7f685b48
philblanchard <phil@philblanchard.com>
2019-08-23 13:54:15 -0400

    finished routing and first pass html

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+var friendList = [
+    {
+        name: "Phil",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Gmail_Icon.svg/1024px-Gmail_Icon.svg.png",
+        scores: [
+            1,
+            2,
+            3,
+            4,
+            5,
+            6,
+            7,
+            8,
+            9,
+            10
+        ]
+    }
+]
+
+
+module.exports = friendList;
