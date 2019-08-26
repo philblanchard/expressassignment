367ef4b44f419d85d865e5b438d4f3a641e23d6c
philblanchard <phil@philblanchard.com>
2019-08-26 14:38:53 -0400

    added matching js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,10 @@
 var friendData = require('../data/friends')
-
+var prevAbs = 0;
+var tempAbs = 0;
+var bestMatch = [];
+calcAbs = (a , b) => {
+    return Math.abs(a - b)
+}
 module.exports = function(app){
     app.get("/api/friends", function(req, res) {
         res.json(friendData)
@@ -7,7 +12,20 @@ module.exports = function(app){
 
 
     app.post("/api/friends", function(req, res) {
+        for (i in friendData){
+            tempAbs = 0;
+            for (z = 0; z < friendData[i].scores.length; z++) {
+                tempAbs+= calcAbs(friendData[i].scores[z], req.body.scores[z] )
+            }
+            prevAbs = tempAbs
+            console.log(tempAbs)
+            if (tempAbs <= prevAbs){
+                bestMatch = friendData[i]
+            }
+        }
+
+
         friendData.push(req.body)
-        res.json(true)
+        res.json(bestMatch)
     })
 }
